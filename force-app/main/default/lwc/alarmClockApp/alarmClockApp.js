45b27814c2395735ecbba845d88fa3c6c8e24afe
Gopi Vallabh Agarwal <agarwalgopi.21599@gmail.com>
2023-08-15 10:17:28 +0530

    Completed the Alarm app

--- force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
+++ force-app/main/default/lwc/alarmClockApp/alarmClockApp.js
@@ -2,9 +2,30 @@ import { LightningElement } from 'lwc';
 import AlarmClockAssets from '@salesforce/resourceUrl/AlarmClockAssets'
 export default class AlarmClockApp extends LightningElement {
     clockImage = AlarmClockAssets+'/AlarmClockAssets/clock.png'
+    ringtone  = new Audio (AlarmClockAssets+'/AlarmClockAssets/Clocksound.mp3')
     currentTime = ''
+    hours = []
+    minutes= []
+
+    hourSelected
+    minSelected
+    meridinSelected=''
+    alarmTime
+    isAlarmSet = false
+    isAlarmTriggered= false
+
+    get isFieldNotSelected(){
+        return !(this.hourSelected && this.minSelected && this.meridinSelected)
+    }
+
+    get shakeImage(){
+        return this.isAlarmTriggered ? 'shake':''
+    }
+
     connectedCallback(){
         this.currentTimeHandler()
+        this.createHoursOptions()
+        this.createMinutesOptions()
     }
 
     currentTimeHandler(){
@@ -27,8 +48,71 @@ export default class AlarmClockApp extends LightningElement {
 
         this.currentTime = `${hour}:${min}:${sec} ${ampm}`
 
-        }, 1000)
+        if(this.alarmTime === `${hour}:${min} ${ampm}`){
+            console.log("Alarm Trigerred")
+            this.isAlarmTriggered=true
+            this.ringtone.play()
+            this.ringtone.loop=true
+        }
 
-        
+        }, 1000) 
     }
-}
+
+    createHoursOptions(){
+        for(let i=1;i<=12;i++){
+            let val = i<10 ? "0"+i: i 
+            this.hours.push(val)
+        }
+    }
+
+    createMinutesOptions(){
+        for(let i=0;i<=59;i++){
+            let val = i<10 ? "0"+i: i 
+            this.minutes.push(val)
+        }
+    }
+
+    optionhandler(event){
+        const {label,value}=event.detail
+        if(label==="Hour(s)"){
+            this.hourSelected = value
+        }else if(label==="Minute(s)"){
+            this.minSelected = value
+        }
+        console.log(this.hourSelected)
+        console.log(this.minSelected)
+    }
+
+    ampmchange(event){
+        const {name,value}=event.target
+        console.log("Bakchodi")
+        console.log("event.target",event.target)
+        this.meridinSelected=value;
+
+        // if(name==="am"){
+        //     this.meridinSelected="AM";
+        // }
+        // else if(name==='pm'){
+        //     this.meridinSelected=value;
+        // }
+        console.log(this.meridinSelected)
+    }
+
+    setAlarmHandler(){
+        this.alarmTime = `${this.hourSelected}:${this.minSelected} ${this.meridinSelected}`
+        this.isAlarmSet=true
+    }
+
+    clearAlarmHandler(event){
+        this.isAlarmSet = false
+        this.alarmTime = ''
+        this.isAlarmTriggered=false
+        const elemets = this.template.querySelectorAll('c-clock-dropdown')
+        Array.from(elemets).forEach(elemet=>{
+            elemet.reset('')
+        })
+        const val = event.target
+        this.meridinSelected= val
+        this.ringtone.pause() 
+    }
+} 
