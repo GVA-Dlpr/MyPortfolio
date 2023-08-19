924f53384dfd4971488495c1054b1170ad41c128
Gopi Vallabh Agarwal <agarwalgopi.21599@gmail.com>
2023-08-19 16:25:25 +0530

    Styling completed for app

--- force-app/main/default/lwc/currencyConvertorApp/currencyConvertorApp.js
+++ force-app/main/default/lwc/currencyConvertorApp/currencyConvertorApp.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
+import { countryCodeList } from 'c/countryCodeList'
+import currencyConverterAssets from '@salesforce/resourceUrl/currencyConverterAssets'
 
-export default class CurrencyConvertorApp extends LightningElement {}
+export default class CurrencyConvertorApp extends LightningElement {
+    currencyImage = currencyConverterAssets +'/currencyConverterAssets/currency.svg'
+    countryList = countryCodeList
+    countryFrom = "INR"
+    countryTo = "USD"
+
+    handleChange(event){
+        const {name, value} = event.target
+        console.log("name",name)
+        console.log("value",value)
+        this[name]=value
+    }
+}
