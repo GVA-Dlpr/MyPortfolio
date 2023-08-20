0a70fce9f3313edd04b4815083c17e6139d651ca
Gopi Vallabh Agarwal <agarwalgopi.21599@gmail.com>
2023-08-20 08:34:59 +0530

    Currency Convertor app completed

--- force-app/main/default/lwc/currencyConvertorApp/currencyConvertorApp.js
+++ force-app/main/default/lwc/currencyConvertorApp/currencyConvertorApp.js
@@ -7,11 +7,37 @@ export default class CurrencyConvertorApp extends LightningElement {
     countryList = countryCodeList
     countryFrom = "INR"
     countryTo = "USD"
+    amount =''
+    result
+    error
 
     handleChange(event){
         const {name, value} = event.target
-        console.log("name",name)
-        console.log("value",value)
+        // console.log("name",name)
+        // console.log("value",value)
+        console.log(this.countryFrom)
+        console.log(this.countryTo)
         this[name]=value
+        this.result=''
+        this.error=''
+    }
+
+    submitHandler(event){
+        event.preventDefault()
+        this.convert()
+    }
+
+    async convert(){
+        const API_URL = `https://api.exchangerate.host/convert?from=${this.countryFrom}&to=${this.countryTo}`
+        try{
+            const data = await fetch(API_URL)
+            const jsonData = await data.json()
+            console.log(jsonData)
+            this.result = (Number(this.amount) * jsonData.result).toFixed(2)
+            console.log(this.result)
+        }catch(error){
+            console.log(error)
+            this.error="An error occured. Try again!!./"
+        }
     }
 }
